16aed5ba38859435d7d8e8e365c13f49d5065474
anuj bhatt <63633305+anujbhatt-dev@users.noreply.github.com>
2020-04-25 20:46:46 +0530

    Update sim.js

--- sim.js
+++ sim.js
@@ -1,7 +1,11 @@
 $(".formIn").hide();
 $(".send-req").click(function () {
   $(".formIn").slideToggle();
+
 });
 $(".formbutton").click(function () {
   $(".formIn").slideToggle();
+});
+$(".formbutton").click(function () {
+  $(".send-req").text("requested").css("background","green").effect('shake');
 });
